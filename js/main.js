6ec1c5caa01e0731a441c3dc73db4d61e81a7839
gloirem690-pixel <gloirem690@gmail.com>
2026-03-14 06:39:22 +0100

    Create js/main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Menu responsive pour mobile
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (menuToggle) {
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+}
